2331b4ee44ecf0fc4853c532c3ad4a4772453dee
Vaibhaw-987 <computergpt987@gmail.com>
2026-08-18 18:52:00 +0530

    Update ContractStatus.tsx

--- components/ContractStatus.tsx
+++ components/ContractStatus.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react"; 
 import { POOL_ADDRESS } from "@/lib/stellar/contracts";
 import { simulateContractRead } from "@/lib/stellar";
 import { scValToNative } from "@stellar/stellar-sdk";
